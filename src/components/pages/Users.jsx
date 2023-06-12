1e84c1e5b133afc5fad1c108f7c0f2f5c7b92891
[Gabriel <118690659+gabriieelmonteiro@users.noreply.github.com>
2023-06-12 20:25:16 -0300

    Reaproveirar header e footer como template

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -2,23 +2,14 @@
 
 import React from "react";
 
-import logo from "../../images/logo.svg";
 import users1 from "../../images/placeholders/user-1.jpg";
 import users2 from "../../images/placeholders/user-2.jpg";
 import users3 from "../../images/placeholders/user-3.jpg";
+import Default from "../templates/Default";
 
 export default function Users() {
   return (
-    <div className="wrapper">
-      <div className="app-header">
-        <div className="app-header__logo">
-          <img src={logo} className="responsive" alt="" />
-        </div>
-        <div className="app-header__menu">
-          <i className="fa fa-bars"></i>
-        </div>
-      </div>
-
+    <Default>
       <div className="users">
         <h1>Users</h1>
 
@@ -43,8 +34,6 @@ export default function Users() {
           </a>
         </div>
       </div>
-
-      <div className="app-footer">Módulo React :: Full Stack Development</div>
-    </div>
+    </Default>
   );
 }
